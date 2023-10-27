ed70dbaeca8e8524ed5daa95beb54c3394b2bb89
KaiqueGovani <kaique.govani@hotmail.com>
2023-10-27 00:10:25 -0300

    TODO Jota

--- src/controller/GerenciamentoProdutos.java
+++ src/controller/GerenciamentoProdutos.java
@@ -4,6 +4,9 @@ package controller;
  * GerenciamentoProdutos
  */
 public class GerenciamentoProdutos {
-
-    
+    //! Existem métodos semelhantes no gerenciamento de clientes, olha lá
+    //TODO Método para cadastrar produtos, será usado na op. 4 do Menu
+    //TODO Método que retorne uma lista (ArrayList) de produtos "listarProdutos"
+    //TODO Método que Busca nos produtos um produto pelo nome e o retorna
+    //TODO Método que retorna uma lista produtos que são perecíveis e já venceram em relação a data de hoje
 }
