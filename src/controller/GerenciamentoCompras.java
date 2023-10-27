20bfbc1c77487a3f4b0a3e135f49024c6b862992
KaiqueGovani <kaique.govani@hotmail.com>
2023-10-27 00:10:17 -0300

    TODO Milton

--- src/controller/GerenciamentoCompras.java
+++ src/controller/GerenciamentoCompras.java
@@ -4,5 +4,10 @@ package controller;
  * GerenciamentoCompras
  */
 public class GerenciamentoCompras {
-    
+    //! Existem métodos semelhantes no gerenciamento de clientes, olha lá
+    //TODO método para Cadastrar uma nova Compra
+    //TODO Método que retorne a lista de compras como um new ArrayList
+    //TODO Método que Busca uma compra pelo identificador e a retorna
+    //TODO Método que retorna uma lista de compras que estão com o valor faltante maior que 0
+    //TODO Método que atualiza o valor pago de uma compra
 }
