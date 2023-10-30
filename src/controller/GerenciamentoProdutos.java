117224b14d3381be89820aa98851b1059da8c55d
KaiqueGovani <54339985+KaiqueGovani@users.noreply.github.com>
2023-10-30 01:19:55 -0300

    [GProdutos] Revisão e Correção

--- src/controller/GerenciamentoProdutos.java
+++ src/controller/GerenciamentoProdutos.java
@@ -1,39 +1,54 @@
 package controller;
 
-private List<Produto> produtos;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
-public GerenciamentoProdutos() {
-    this.produtos = new ArrayList <>();
-}
+import model.Produto;
+import model.ProdutoPerecivel;
 
-public boolean cadastrarProdutos(Produto produto) {
-    if(buscarPeloNome(produto.pegarNome())){
-        System.out.prinln("Produto já existe");
-        return false;
+// ! Estava faltando importar os módulos aqui em cima !
+
+
+//! Seria bom adicionar docstrings nos métodos, para facilitar a leitura do código.
+public class GerenciamentoProdutos {
+
+    private List<Produto> produtos;
+
+    public GerenciamentoProdutos() {
+        this.produtos = new ArrayList<>();
     }
-    produtos.add(produto);
-    System.out.println("Produto adicionado com sucesso");
-    System.out.println(produto.paraString());
-    return true;
+
+    public boolean cadastrarProdutos(Produto produto) {
+        if (buscarPeloNome(produto.pegarNome())) {
+            System.out.println("Produto já existe");
+            return false;
+        }
+        produtos.add(produto);
+        System.out.println("Produto adicionado com sucesso");
+        System.out.println(produto.paraString());
+        return true;
+    }
+
+    public List<Produto> listarProdutos() {
+        return this.produtos; // !Criar cópia da lista para evitar que a lista original seja modificada.
+    }
+
+    public boolean buscarPeloNome(String nome) {
+        return produtos.stream()
+                .anyMatch(produto -> produto.pegarNome().equals(nome));
+    }
+
+    public List<ProdutoPerecivel> listarProdutosPereciveis() {
+        LocalDate dataAtual = LocalDate.now();
+        //! Há maneiras mais simples e visíveis de fazer isso, igual em gerenciamentoClientes.
+        return produtos.stream()
+                .filter(produto -> produto instanceof ProdutoPerecivel)
+                .map(produto -> (ProdutoPerecivel) produto) // ! Talvez não faça sentido fazer esse cast, já que a linha de cima já garante que o produto é perecível.
+                .filter(produtoPerecivel -> produtoPerecivel.estaVencido()
+                        && produtoPerecivel.pegarDataDeValidade().isBefore(dataAtual)) //!Está vencido já é o suficiente, essa parte não tem necessidade.
+                .collect(Collectors.toList());
+    }
+
 }
-
-public List<Produto> listarProdutos() {
-    return this.produtos;
-}
-
-public boolean buscarPeloNome(String nome) {
-    return produtos.stream()
-            .anyMatch(produto -> produto.pegarNome().equals(nome));
-}
-
-public List<ProdutoPerecivel> listaProdutosPereciveis() {
-    LocalDate dataAtual = LocalDate.now();
-
-    return produtos.steam()
-            .filter(produto -> produto instanceof ProdutoPerecivel)
-            .map(produto -> (ProdutoPerecivel) produto)
-            .filter(produtoPerecivel -> produtoPerecivel.estaVencido() && produtoPerecivel.pegarDataDeValidade().isBefore(dataAtual))
-            .collect(Collectors.toList());
-}
-
-
