2fa967c9a2558754e9a0d637f453e116a7f7657a
KaiqueGovani <kaique.govani@hotmail.com>
2023-10-30 20:01:38 -0300

    [IU] Alterando gClientes e pegarValorDigitado

--- src/view/InterfaceUsuario.java
+++ src/view/InterfaceUsuario.java
@@ -3,15 +3,18 @@ package view;
 import javax.swing.JOptionPane;
 
 import controller.GerenciamentoClientes;
+import controller.GerenciamentoCompras;
 
 /**
  * InterfaceUsuario
  */
 public class InterfaceUsuario {
-    private GerenciamentoClientes gc;
+    private GerenciamentoClientes gClientes;
+    private GerenciamentoCompras gCompras;
 
-    public InterfaceUsuario(GerenciamentoClientes gc) {
-        this.gc = gc;
+    public InterfaceUsuario(GerenciamentoClientes gClientes, GerenciamentoCompras gCompras) {
+        this.gClientes = gClientes;
+        this.gCompras = gCompras;
     }
 
     public static void mostrarErro(String msg, String titulo) {
@@ -41,7 +44,7 @@ public class InterfaceUsuario {
      * @param titulo
      * @return String do valor digitado
      */
-    public String pegaValorDigitado(String msg, String titulo) {
+    public static String pegarValorDigitado(String msg, String titulo) {
         String valorDigitado;
         do {
             valorDigitado = JOptionPane.showInputDialog(null, msg, titulo, JOptionPane.PLAIN_MESSAGE);
@@ -85,22 +88,22 @@ public class InterfaceUsuario {
             try {
                 int opcaoInt = Integer.parseInt(valorSelecionado);
                 if (opcaoInt >= 1 && opcaoInt <= 2) {
-                    String nome = pegaValorDigitado("Digite o nome do cliente:", titulo);
-                    String rua = pegaValorDigitado("Digite a rua do cliente:", titulo);
-                    int numero = Integer.parseInt(pegaValorDigitado("Digite o número da casa:", titulo));
-                    String bairro = pegaValorDigitado("Digite o bairro do cliente:", titulo);
-                    String cep = pegaValorDigitado("Digite o CEP do cliente:", titulo);
-                    String cidade = pegaValorDigitado("Digite a cidade do cliente:", titulo);
-                    String estado = pegaValorDigitado("Digite o estado do cliente:", titulo);
+                    String nome = pegarValorDigitado("Digite o nome do cliente:", titulo);
+                    String rua = pegarValorDigitado("Digite a rua do cliente:", titulo);
+                    int numero = Integer.parseInt(pegarValorDigitado("Digite o número da casa:", titulo));
+                    String bairro = pegarValorDigitado("Digite o bairro do cliente:", titulo);
+                    String cep = pegarValorDigitado("Digite o CEP do cliente:", titulo);
+                    String cidade = pegarValorDigitado("Digite a cidade do cliente:", titulo);
+                    String estado = pegarValorDigitado("Digite o estado do cliente:", titulo);
 
                     switch (opcaoInt) {
                         case 1: // Pessoa Física
-                            String cpf = pegaValorDigitado("Digite o CPF do cliente:", titulo);
-                            int qtdMaxParcelas = Integer.parseInt(pegaValorDigitado("Digite a quantidade máxima de parcelas do cliente:", titulo));
+                            String cpf = pegarValorDigitado("Digite o CPF do cliente:", titulo);
+                            int qtdMaxParcelas = Integer.parseInt(pegarValorDigitado("Digite a quantidade máxima de parcelas do cliente:", titulo));
 
-                            gc.cadastrarCliente(gc.criarPessoaFisica(
+                            gClientes.cadastrarCliente(gClientes.criarPessoaFisica(
                                 nome,
-                                gc.criarEndereco(rua, numero, bairro, cep, cidade, estado),
+                                gClientes.criarEndereco(rua, numero, bairro, cep, cidade, estado),
                                 cpf,
                                 qtdMaxParcelas
                             ));
@@ -108,13 +111,13 @@ public class InterfaceUsuario {
                             mostrarMensagem("Cliente cadastrado com sucesso!", titulo);
                             break;
                         case 2: // Pessoa Jurídica
-                            String cnpj = pegaValorDigitado("Digite o CNPJ do cliente:", titulo);
-                            String razaoSocial = pegaValorDigitado("Digite a razão social do cliente:", titulo);
-                            int prazoPagamento = Integer.parseInt(pegaValorDigitado("Digite o prazo de pagamento do cliente:", titulo));
+                            String cnpj = pegarValorDigitado("Digite o CNPJ do cliente:", titulo);
+                            String razaoSocial = pegarValorDigitado("Digite a razão social do cliente:", titulo);
+                            int prazoPagamento = Integer.parseInt(pegarValorDigitado("Digite o prazo de pagamento do cliente:", titulo));
                             
-                            gc.cadastrarCliente(gc.criarPessoaJuridica(
+                            gClientes.cadastrarCliente(gClientes.criarPessoaJuridica(
                                 nome,
-                                gc.criarEndereco(rua, numero, bairro, cep, cidade, estado),
+                                gClientes.criarEndereco(rua, numero, bairro, cep, cidade, estado),
                                 cnpj,
                                 razaoSocial,
                                 prazoPagamento
@@ -139,8 +142,8 @@ public class InterfaceUsuario {
 
     public void deletarClientePorDocumento() {
         String titulo = "Deletar Cliente";
-        String documento = pegaValorDigitado("Digite o documento do cliente:", titulo);
-        if (gc.deletarPeloDocumento(documento)) {
+        String documento = pegarValorDigitado("Digite o documento do cliente:", titulo);
+        if (gClientes.deletarPeloDocumento(documento)) {
             mostrarMensagem("Cliente deletado com sucesso!", titulo);
         } else {
             mostrarErro("Cliente não encontrado!", titulo);
@@ -149,11 +152,17 @@ public class InterfaceUsuario {
 
     public void deletarClientePorNome(){
         String titulo = "Deletar Cliente";
-        String nome = pegaValorDigitado("Digite o nome do cliente:", titulo);
-        if (gc.deletarPeloNome(nome)) {
+        String nome = pegarValorDigitado("Digite o nome do cliente:", titulo);
+        System.out.println(nome);
+        if (gClientes.deletarPeloNome(nome)) {
             mostrarMensagem("Cliente deletado com sucesso!", titulo);
         } else {
             mostrarErro("Cliente não encontrado!", titulo);
         }
     }
+
+    public void efetuarCompra() {
+        //TODO 5. Efetuação de uma compra
+    }
+
 }
